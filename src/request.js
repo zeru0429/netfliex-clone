c64ca32868f7fecd28ed2444b3a3637e261b7b64
zeru0429 <mihiretutigistu@gmail.com>
2023-08-18 10:01:26 +0300

    .env is added

--- src/request.js
+++ src/request.js
@@ -1,6 +1,5 @@
-const API_KEY = "177269945e48f57f144fce22779a7dd9";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const requests = {
-   
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
