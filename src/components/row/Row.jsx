8731a3ad0f4128cc6afb808e47a8963a5253dcbe
zeru0429 <mihiretutigistu@gmail.com>
2023-08-18 09:08:44 +0300

    button add completed

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -1,34 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../axios";
 import "./row.css";
-import YouTube from "react-youtube"
-import movieTrailer from "movie-trailer"
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
+
+
 function Row({ title, fetchUrl, isposter }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("")
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const [scrollPosition, setScrollPosition] = useState(0);
   const scrollAmount = 500; // Adjust this value to control the scroll distance
 
   const scrollLeft = () => {
-  const row = document.getElementById("row");
-  if (row) {
-    row.scrollLeft -= scrollAmount;
-    setScrollPosition(row.scrollLeft);
-  }
+    const row = document.getElementById(`row-${title}`);
+    if (row) {
+      row.scrollLeft -= scrollAmount;
+      setScrollPosition(row.scrollLeft);
+    }
   };
 
-const scrollRight = () => {
-  const row = document.getElementById("row");
-  if (row) {
-    row.scrollLeft += scrollAmount;
-    setScrollPosition(row.scrollLeft);
-  }
-};
-
+  const scrollRight = () => {
+    const row = document.getElementById(`row-${title}`);
+    if (row) {
+      row.scrollLeft += scrollAmount;
+      setScrollPosition(row.scrollLeft);
+    }
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -40,54 +43,56 @@ const scrollRight = () => {
   }, [fetchUrl]);
 
   const opts = {
-    heighr: "390",
+    height: "390",
     width: "100%",
     playerVars: {
       autoplay: 1,
-    }
-  }
+    },
+  };
 
   const handleClick = (movie) => {
-    if(trailerUrl){
-      setTrailerUrl('')
-    }else {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
       movieTrailer(movie?.title || "")
-      .then((url) => {
-        const urlParams = new URLSearchParams(new URL(url).search)
-        setTrailerUrl(urlParams.get('v'))
-      })
-      .catch((error) => console.log(error))
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
     }
-  }
- return (
-  <div className="row">
-    <h2>{title}</h2>
-    <div className="row__buttons">
-      <button className="scroll-button" onClick={scrollLeft}>
-        &lt;
-      </button>
-      <button className="scroll-button" onClick={scrollRight}>
-        &gt;
-      </button>
+  };
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__buttons">
+        <button className="scroll-button" onClick={scrollLeft}>
+         <KeyboardArrowLeftIcon  className="icons"/>
+        </button>
+        <div className="row__posters" id={`row-${title}`}>
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isposter && "row__posterLarge"}`}
+              src={`${base_url}${
+                isposter ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <button className="scroll-button" onClick={scrollRight}>
+          <KeyboardArrowRightIcon className="icons"/>
+        </button>
+      </div>
+
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
     </div>
-    <div className="row__posters" id="row">
-      {movies.map((movie) => (
-        <img
-          key={movie.id}
-          onClick={() => handleClick(movie)}
-          className={`row__poster ${isposter && "row__posterLarge"}`}
-          src={`${base_url}${
-            isposter ? movie.poster_path : movie.backdrop_path
-          }`}
-          alt={movie.name}
-        />
-      ))}
-    </div>
-    <div style={{ padding: "40px" }}>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-    </div>
-  </div>
-);
+  );
 }
 
 export default Row;
